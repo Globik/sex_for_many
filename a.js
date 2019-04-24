e552b79e2bb6176d967604cad08d262b7c533981
Globik <gru5@yandex.ru>
2019-04-24 14:17:00 -0400

    loop of

--- a.js
+++ a.js
@@ -53,26 +53,37 @@ el.send(JSON.stringify(obj));
 
 
 function send_target_trans(trans, obj, sid){
-console.log("trg sessId: ", trans);
-wss.clients.forEach(function(el){// better for k of wss.clients
-if(el.trans==trans){
-console.log("trg Yes. It's target! ",el.trans, trans);
-if(sid==1){if(el.sid==0)el.sid=obj.data.id;}else if(sid==2){el.sid=0}
-if(el.readyState===WebSocket.OPEN)el.send(JSON.stringify(obj));	
-}	
-});
-}
-function send_target_sess(session_id, obj){
-console.log("send target sess: ", session_id);
-//wss.clients.forEach(function(el){// better for k of wss.clients
+console.log("send_target_trans(): ", trans);
 for(var el of wss.clients){
-if(el.sid== session_id){
+console.log("OF el.trans: ",el.trans);
+//wss.clients.forEach(function(el){
+//console.log('el trans', el.trans);
+if(el.trans==trans){
+console.log("Yes. It's target trans! ",el.trans, trans,'el.sid ', el.sid,' sid ', sid);
+if(sid==1){
+if(el.sid==0){
+el.sid=obj.data.id;
+console.log("Attaching a session_id");
+}
+	
+}else if(sid==2){console.log('Detaching a session id');el.sid=0}
+console.log("NOW SENDING ",obj);
+if(el.readyState===WebSocket.OPEN)el.send(JSON.stringify(obj));
+break;	
+}
+}
+//)
+}
+
+function send_target_sess(session_id, obj){
+console.log("send_target_sess(): ", session_id);
+for(var el of wss.clients){
+if(el.sid == session_id){
 console.log("Yes, session matches. ",el.sid, session_id);
 if(el.readyState===WebSocket.OPEN)el.send(JSON.stringify(obj));
 break;	
 }	
 }
-//);
 }
 
 
@@ -90,12 +101,13 @@ let len=a.length;
 l.transi=a[len-1];
 let c=Number(a[len-1]);
 if(c==10){
-console.log("create session");
+console.log("'create' session");
 sess=1;//ws.sid=l.data.id => new session
-}else if(c==1){
+}else if(c==11){
+console.log("'destroy' session_id");
 sess=2;//ws.sid=0
 }
-send_target_trans(a[len-2],l, sess);
+send_target_trans(a[len-2], l, sess);
 }
 if(l.janus=="media"){
 console.log("media is here",l);	 
@@ -113,7 +125,7 @@ ws.hid=0;
 
 wsend({typ:"usid", msg: "Hi from server!"});
 ws.on('message', function d_msg(msg){
-console.log("msg came: ",msg);
+console.log("msg came: ", msg);
 let l;var sens_to_clients=0;
 try{
 l=JSON.parse(msg);	
@@ -123,10 +135,11 @@ sub.send(msg);
 send_to_clients=1;
 }
 if(l.typ=="onuser"){
-console.log("MSG type: ", l.type);
+console.log("Typ: ", l.typ);
 ws.trans=l.username;
 send_to_clients=1;	
 }else if(l.typ=="onair"){
+console.log("ON AIR!");
 l.typ="atair";//for subscribers signal
 broadcast_room_no_me(ws, l);
 send_to_clients=1;	
