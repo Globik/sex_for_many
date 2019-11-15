80905805ba66a1f275944d70dc7774a5941ae65c
Globik <gru5@yandex.ru>
2019-11-15 07:27:59 -0500

    n

--- public/js/video_chat_janus.js
+++ public/js/video_chat_janus.js
@@ -309,11 +309,11 @@ wsend(d);
 }else if(a.typ=="on_btc"){
 	//donation came
 console.log(a);
-btcc.textContent=a.btc_amt;
-alert('a.btc_all: '+a.btc_all);	
+btcc.textContent=a.btc_all;
+//alert('a.btc_all: '+a.btc_all);	
 let obj={};
 obj.from="unknown";
-obj.msg="btc came: "+a.btc_amt;
+obj.msg="satoshi sent: "+a.btc_amt;
 insert_message(obj);
 }else{}
 
