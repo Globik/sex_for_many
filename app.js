1ce2c215d1ee61edd891be78bb569661ecb06dbf
Globik <gru5@yandex.ru>
2020-04-13 07:46:22 -0400

    jj

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const fs = require('fs');
 const is_ssl_http = false;
 //const DB_URL = 'postgress://globik:null@localhost:5432/test';
 //const DB_URL=process.env.DATABASE_URL;//for heroku
-const DB_URL='postgress://globi:null@45.89.67.145:5432/globi';
+//const DB_URL='postgress://globi:null@45.89.67.145:5432/globi';
 
-
+const DB_URL='postgress://root:null@145.89.67.145/globi';
 
 
 
